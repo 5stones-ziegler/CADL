06bcdcff7456f385c36d7dfc27ed40b4a96f0cab
Lee <5stoneschat@gmail.com>
2022-12-23 13:20:32 -0500

    metrics add to seasons

--- demodb/models/tickets/seasons.sql
+++ demodb/models/tickets/seasons.sql
@@ -1,17 +1,24 @@
-SELECT customer_id, 
-season_code, 
-item_code, 
-seat_loc, 
-price_level_full, 
-price_type_desc,
-ROUND(avg(num_seats_event),0)::int as seat_count,
-max(item_amt) as cost,
-max(item_payment) as payments,
-(max(item_amt)-max(item_payment)) as balance_due,
-min(orig_datetime) as purchase_datetime
-FROM {{ source('raw', 't_ticket_w')}}
-WHERE CONCAT(season_code, '-', item_code) IN (
-    SELECT event_id FROM {{ ref('seed_packages') }} 
-    WHERE item_type = 'Full Season'
+with basics_cte AS (SELECT t.customer_id, 
+t.season_code,
+t.item_code, 
+s.renewal_opendate,
+s.renewal_deadline,
+t.seat_loc, 
+t.price_level_full, 
+t.price_type_desc,
+ROUND(avg(t.num_seats_event),0)::int as seat_count,
+max(t.item_amt) as cost,
+max(t.item_payment) as payments,
+(max(t.item_amt)-max(t.item_payment)) as balance_due,
+min(t.orig_datetime) as purchase_datetime
+FROM {{ source('raw', 't_ticket_w')}} t
+INNER JOIN {{ ref('seed_packages') }} s
+ON CONCAT(t.season_code, '-', t.item_code) = s.event_id
+AND lower(s.item_type) = 'full season'
+GROUP BY 1,2,3,4,5,6,7,8
 )
-GROUP BY 1,2,3,4,5,6
+SELECT *,
+sum(seat_count) OVER (PARTITION BY season_code ORDER BY purchase_datetime asc) as renewed_seats_to_date,
+extract(day from (date_trunc('day', renewal_deadline) - date_trunc('day', purchase_datetime))) as renewed_days_before_deadline,
+extract(day from (date_trunc('day', purchase_datetime) - date_trunc('day', renewal_opendate))) as renewed_days_after_open
+FROM basics_cte
